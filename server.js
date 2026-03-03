1c53afc6d184d332bcfe7f1b2a239e92b68504da
Luis Rotich <luisrotich@gmail.com>
2026-03-03 15:39:22 +0300

    Add server for Railway deployment

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Serve all static files (HTML, CSS, JS, images)
 app.use(express.static(path.join(__dirname)));
 
+// Serve index.html on root request
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.listen(PORT, () => {
-  console.log("Server running");
+  console.log(`LemonadeChatBot running on port ${PORT}`);
 });
